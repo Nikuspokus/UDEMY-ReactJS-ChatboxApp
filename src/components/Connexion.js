fbdc054b84b891f1b6ea96b3647b2dcd3cc84258
Marquillier <nicolas.marquillier@gmail.com>
2021-06-16 15:42:42 +0200

    Chap 44 - Changement de page avec React Router V4

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 
 class Connexion extends Component {
   state = {
-    pseudo: ''
+    pseudo: '',
+    goToChat: false
+  }
+
+  handleChange = event => {
+    const pseudo = event.target.value
+    this.setState({ pseudo })
+  }
+
+  handleSubmit = event => {
+    // annule levenement par defaut
+    event.preventDefault()
+    this.setState({ goToChat: true })
   }
 
   render() {
+    if(this.state.goToChat) {
+      return <Redirect to={`/pseudo/${this.state.pseudo}`}></Redirect>
+    }
+
+
     return (
       <div className='connexionBox'>
-        <form className="connexion">
+        <form className="connexion" onSubmit={this.handleSubmit}>
         <div className="wrapper">
           <p>Coding is</p>
           <div className="words">
@@ -21,6 +39,7 @@ class Connexion extends Component {
         </div>
           <input 
             value={this.state.pseudo}
+            onChange={this.handleChange}
             placeholder='Pseudo'
             type="text" 
             required
