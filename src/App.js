2fb9ed0a9093e72e693639f5d97a369539c6ce8c
Marquillier <nicolas.marquillier@gmail.com>
2021-06-16 14:20:58 +0200

    Chap 42 - Projet 2 - Formulaire de Connexion

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <h2>Chatbox App</h2>
+      <div className='box'>
+      </div>
+      </Fragment>
+    )
+  }
+}
+
+export default App
