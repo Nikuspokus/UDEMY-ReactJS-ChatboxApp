d2bf1109f26f9570e75106c5f50819f886dac7d7
Marquillier <nicolas.marquillier@gmail.com>
2021-06-16 15:20:40 +0200

    Modification perso sur je projet ajour d'une animation CSS

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class Connexion extends Component {
+  state = {
+    pseudo: ''
+  }
+
   render() {
     return (
       <div className='connexionBox'>
         <form className="connexion">
+        <div className="wrapper">
+          <p>Coding is</p>
+          <div className="words">
+            <span>fantastic</span>
+            <span>interesting</span>
+            <span>awesome</span>
+            <span>incredible</span>
+            <span>fantastic</span>
+          </div>
+        </div>
           <input 
+            value={this.state.pseudo}
             placeholder='Pseudo'
             type="text" 
             required
