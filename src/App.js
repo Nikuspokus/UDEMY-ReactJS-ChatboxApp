45b545b302d0afb811185e445a6688884f210f86
Marquillier <nicolas.marquillier@gmail.com>
2021-06-17 11:03:24 +0200

    Chap 47 - Gestion de nouveaux Events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React, { Component, } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Message from "./components/Message";
 
 import Formulaire from "./components/Formulaire";
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo,
+  };
+
+  addMessage = (message) => {
+    const messages = { ...this.state.messages };
+    messages[`message-${Date.now()}`] = message;
+    this.setState({ messages });
+  };
+
   render() {
     return (
-        <div className="box">
-          <div>
-            <div className="messages">
-              <Message />
-              <Message />
-              <Message />
-            </div>
+      <div className="box">
+        <div>
+          <div className="messages">
+            <Message />
+            <Message />
+            <Message />
           </div>
-          <Formulaire/>
         </div>
-
+        <Formulaire 
+          length={140}
+          pseudo = { this.state.pseudo }
+          addMessage = {this.addMessage}
+        />
+      </div>
     );
   }
 }
